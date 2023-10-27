7053922d72a440b647e8ea08f4e9189fc4ee8ab1
Sumanta <sumanta9090@gmail.com>
2023-10-28 02:58:19 +0530

    Email model added.

--- rococo/models/__init__.py
+++ rococo/models/__init__.py
@@ -1,7 +1,7 @@
-from .versioned_model import VersionedModel
-
+from .email import Email
 from .login_method import LoginMethod
 from .organization import Organization
 from .otp_method import OtpMethod
 from .person import Person
 from .person_organization_role import PersonOrganizationRole
+from .versioned_model import VersionedModel

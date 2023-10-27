ac101f2db1faf6e0a23a043092c2657a55775bf8
Sumanta <sumanta9090@gmail.com>
2023-10-28 04:31:09 +0530

    - Email repositories updated. - Corrected code implementation.

--- rococo/repositories/base_repository.py
+++ rococo/repositories/base_repository.py
@@ -11,24 +11,42 @@ class BaseRepository:
         self.adapter = adapter
         self.model = model
         self.table_name = model.__name__.lower()
-    
+
+    def _execute_within_context(self, func, *args, **kwargs):
+        """Utility method to execute adapter methods within the context manager."""
+        with self.adapter:
+            return func(*args, **kwargs)
+
     def get_one(self, conditions: Dict[str, Any]) -> Union[VersionedModel, None]:
-        data = self.adapter.get_one(self.table_name, conditions)
+        data = self._execute_within_context(
+            self.adapter.get_one, self.table_name, conditions
+        )
 
         if not data:
             return None
         return self.model.from_dict(data)
 
-    def get_many(self, conditions: Dict[str, Any] = None, sort: List[tuple] = None, 
-                 limit: int = 100) -> List[VersionedModel]:
-        records = self.adapter.get_many(self.table_name, conditions, sort, limit)
+    def get_many(
+        self,
+        conditions: Dict[str, Any] = None,
+        sort: List[tuple] = None,
+        limit: int = 100,
+    ) -> List[VersionedModel]:
+        records = self._execute_within_context(
+            self.adapter.get_many, self.table_name, conditions, sort, limit
+        )
+
+        # If the adapter returned a single dictionary, wrap it in a list
+        if isinstance(records, dict):
+            records = [records]
+
         return [self.model.from_dict(record) for record in records]
 
     def save(self, instance: VersionedModel):
-        print(self.table_name)
         data = instance.as_dict(convert_datetime_to_iso_string=True)
-        return self.adapter.save(self.table_name, data)
-            
+        return self._execute_within_context(self.adapter.save, self.table_name, data)
 
     def delete(self, conditions: Dict[str, Any]) -> bool:
-        return self.adapter.delete(self.table_name, conditions)
+        return self._execute_within_context(
+            self.adapter.delete, self.table_name, conditions
+        )

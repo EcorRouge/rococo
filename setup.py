eec9c500f3e9f3ce1f892e18d5719d43f6d83cca
Sumanta <sumanta9090@gmail.com>
2023-11-08 17:03:25 +0530

    -surreal db repository added. - Bumped version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='0.1.18',
+    version='0.1.19',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

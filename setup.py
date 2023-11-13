e6570e06b9e536226411d88df54b264648748654
afeef <janjua.afeef@gmail.com>
2023-11-14 01:52:15 +0500

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='0.1.21',
+    version='0.1.23',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

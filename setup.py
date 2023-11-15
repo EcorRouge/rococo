6c8c2f16d05293010340b2093088cafd3024facc
asymness <asym1999@gmail.com>
2023-11-16 02:11:03 +0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='0.1.30',
+    version='0.1.31',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

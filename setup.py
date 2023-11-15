b59698b7b4895ff6410a574aa752b32235b2db0e
Matias Garafoni <matias.garafoni@gmail.com>
2023-11-15 16:18:02 -0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='0.1.27',
+    version='0.1.28',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

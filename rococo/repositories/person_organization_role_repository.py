8931fb4bc1596df3df096aeb2a5bb7b6a82ed7d9
sinisarudan <sinisa.rudan@gmail.com>
2023-12-08 18:57:25 +0100

    updates to new SurrealDb Model; find_organizations_by_owner

--- rococo/repositories/person_organization_role_repository.py
+++ rococo/repositories/person_organization_role_repository.py
@@ -12,24 +12,28 @@ class PersonOrganizationRoleRepository(SurrealDbRepository):
         """finds all the organizations that a Person identified by member_id is a member of 
 
         Args:
-            member_id (str): `person.entity_id`
+            member_id (str): `person.id`
             get_member (bool, optional): should a member (Person) be fetched too?. Defaults to False.
 
         Returns:
             List[PersonOrganizationRole]: found PersonOrganizationRole
         """
-        conditions = {"person.entity_id": member_id}
+        conditions = {"person.id": member_id}
         fetch_related: List[str] = ["organization", "person"] if get_member else ["organization"]
         return self.get_many(conditions, fetch_related=fetch_related)
     
-    def find_by_owner(self, owner_id: str, get_owner: bool = False) -> List[PersonOrganizationRole]:
-        # TODO
-        conditions = {"person.entity_id": owner_id}
+    def find_organizations_by_owner(self, owner_id: str, get_owner: bool = False) -> List[PersonOrganizationRole]:
+        """finds all the organizations that a Person identified by member_id is an owner of 
+
+        Args:
+            owner_id (str): `person.id`
+            get_owner (bool, optional): should an owner (Person) be fetched too?. Defaults to False.
+
+        Returns:
+            List[PersonOrganizationRole]: found PersonOrganizationRole-s
+        """
+        print(f"[find_organizations_by_owner] owner_id: {owner_id}", flush=True)
+        OWNER: str = "OWNER"
+        conditions = {"person.id": owner_id, "role": OWNER}
         fetch_related: List[str] = ["organization", "person"] if get_owner else ["organization"]
         return self.get_many(conditions, fetch_related=fetch_related)
-
-    # in the case we chose to have `role` as a `PersonOrganizationRoleEnum` instead of `str`
-    #  def save(self, instance: PersonOrganizationRole, send_message: bool = False) -> PersonOrganizationRole:
-    #     instance_copy = copy.copy(instance)
-    #     instance_copy.role = instance.role.value
-    #     super().save(instance=instance_copy, send_message=send_message)

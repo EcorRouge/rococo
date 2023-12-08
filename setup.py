bfff744496308f8f41213f713ff68b82befc6635
sinisarudan <sinisa.rudan@gmail.com>
2023-12-08 11:04:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='0.1.44',
+    version='0.1.45',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

2b8a34f7f0f2cdeea28ae69fdee7832ed447d6bb
sinisarudan <sinisa.rudan@gmail.com>
2023-12-08 07:56:09 +0100

    organization and role models extended

--- rococo/models/organization.py
+++ rococo/models/organization.py
@@ -12,3 +12,11 @@ class Organization(VersionedModel):
     """An organization model."""
 
     name: str = None
+    code: str = None
+    description: str = None
+    # members (with accompanied roles, including `owner`) are maintained through `PersonOrganizationRole`
+
+    # TODO: to see do we want redundancy, to have a relationship from an `Organization` to its `owner`
+    # despite that `roles` in Organization (including the `owner`) are defined through `PersonOrganizationRole`
+
+    # we could support relationships and hierarchy among organizations

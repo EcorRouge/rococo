cb27dd55d3b01d0783283814c0040db201da5380
sinisarudan <sinisa.rudan@gmail.com>
2023-12-08 07:57:01 +0100

    Update __init__.py

--- rococo/repositories/__init__.py
+++ rococo/repositories/__init__.py
@@ -3,3 +3,5 @@ base repository for rococo
 """
 from .base_repository import BaseRepository
 from .surreal_db_repository import SurrealDbRepository
+from .organization_repository import OrganizationRepository
+from .person_organization_role_repository import PersonOrganizationRoleRepository

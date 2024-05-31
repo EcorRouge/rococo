410bdcb243b9b9aaa2267238b76d00c927364b47
Kote Akhaladze <koteakh@gmail.com>
2024-05-31 07:01:06 +0400

    fix tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='1.0.12',
+    version='1.0.13',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

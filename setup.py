db2f959de0442a0913f677c1cd51437fc9223db0
asymness <asym1999@gmail.com>
2024-10-14 05:02:17 +0500

    Add model validation and experimental type checking

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='1.0.17',
+    version='1.0.18',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

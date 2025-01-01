4603a77d45a3b1c777d8803bd9ac5e9d5c1267b5
asymness <asym1999@gmail.com>
2025-01-01 06:22:45 +0500

    Add plugin and support for Pooled DB connections in Flask

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='1.0.27',
+    version='1.0.28',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

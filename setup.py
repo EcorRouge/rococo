f0de4db5acf2cf139390515f8bc952c1afe13a60
asymness <asym1999@gmail.com>
2025-03-20 20:03:40 +0500

    Add init to migrations.common module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='1.0.32',
+    version='1.0.33',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

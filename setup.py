7a03ad554d40707a83b1ed173d0227a7b8a4494f
Arun Verma <arundevops47@gmail.com>
2025-05-15 20:50:49 +0530

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rococo',
-    version='1.0.34',
+    version='1.0.35',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

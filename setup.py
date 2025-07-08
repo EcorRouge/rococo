ff170553275ef5089a551577a98991f61d9a5098
Mikhail B <mikhail-burilovmv@proton.me>
2025-07-08 20:56:32 +0300

    ROC-70 Another fix to mongodb repository, save() method

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ extras_require["all"] = [
 
 setup(
     name='rococo',
-    version='1.0.38',
+    version='1.0.39',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

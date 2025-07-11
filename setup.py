ab05361a21f3f9af6752702d79e5560a5a2ac28c
Mikhail B <mikhail-burilovmv@proton.me>
2025-07-11 16:56:42 +0300

    ROC-74 Fix mongodb delete() not working

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ extras_require["all"] = [
 
 setup(
     name='rococo',
-    version='1.0.40',
+    version='1.0.41',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

80224c8a1c9064a7266613e054d1a1a60c1c5822
Ilyas Karim <ilyas.datoo@gmail.com>
2025-12-10 14:10:59 +0500

    Bump Version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ extras_require["all"] = [
 
 setup(
     name='rococo',
-    version='1.1.12',
+    version='1.2.0',
     packages=find_packages(),
     url='https://github.com/EcorRouge/rococo',
     license='MIT',

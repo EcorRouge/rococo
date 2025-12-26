00ec763daf188e61d391f60d8b02f0b8ea2d8cea
Ilyas Karim <ilyas.datoo@gmail.com>
2025-12-26 14:48:35 +0500

    Fix tests

--- tests/test_calculated_properties_fix.py
+++ tests/test_calculated_properties_fix.py
@@ -4,7 +4,6 @@ Test cases for calculated properties fix in VersionedModel
 
 import unittest
 from dataclasses import dataclass
-from typing import Optional
 from rococo.models.versioned_model import VersionedModel
 
 
@@ -13,7 +12,7 @@ class TestModelWithCalculatedProperty(VersionedModel):
     """Test model with a calculated property similar to OrganizationImport.error_count"""
 
     count: int = 0
-    errors: Optional[list] = None
+    errors: list = None
 
     def __post_init__(self, _is_partial):
         if self.errors is None:
